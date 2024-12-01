5513eb900fcd9296afe1b09cf58c351fade8ad01
Anurag <128254507+a-mahakal@users.noreply.github.com>
2024-12-01 11:58:31 +0530

    added deploy step

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enables static export
-  basePath: '/weekend-test', // Set the base path for GitHub Pages
-  assetPrefix: '/weekend-test/', // Prefix for assets
+  output: 'export',
 };
 
 export default nextConfig;
