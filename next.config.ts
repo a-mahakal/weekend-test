c7891adf80d491de454daa1fe6cf06b81c3f16ff
Anurag <128254507+a-mahakal@users.noreply.github.com>
2024-12-01 11:44:40 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
-  basePath: '/<weekend-test>', // Set the base path for GitHub Pages
-  assetPrefix: '/<weekend-test>/', // Prefix for assets
+  basePath: '/weekend-test', // Set the base path for GitHub Pages
+  assetPrefix: '/weekend-test/', // Prefix for assets
 };
 
 export default nextConfig;
